7f98ecd2447ebd9caf6ca9f84876c2fbec00c50f
pauljoegeorge <pauljoegeorge@gmail.com>
2020-04-19 20:34:42 +0900

    follow users

---
+++ db/migrate/20200419112720_create_follows.rb
@@ -0,0 +1,9 @@
+class CreateFollows < ActiveRecord::Migration[6.0]
+  def change
+    create_table :follows do |t|
+      t.references :follower, foreign_key: { to_table: :users }, null: false
+      t.references :followed, foreign_key: { to_table: :users }, null: false
+      t.timestamps
+    end
+  end
+end
