8f5ccbbad3efb9ab5349cd27d62ba109f4c3591f
pauljoegeorge <pauljoegeorge@gmail.com>
2020-04-21 19:24:35 +0900

    changing migration code

--- db/migrate/20200419112720_create_follows.rb
+++ db/migrate/20200419112720_create_follows.rb
@@ -1,9 +1,10 @@
 class CreateFollows < ActiveRecord::Migration[6.0]
   def change
     create_table :follows do |t|
+      t.references :following, foreign_key: { to_table: :users }, null: false
       t.references :follower, foreign_key: { to_table: :users }, null: false
-      t.references :followed, foreign_key: { to_table: :users }, null: false
       t.timestamps
     end
+    add_index :follows, [:following_id, :follower_id], unique: true
   end
 end
