c64bca86558e709e3408ad045f905751d63ba903
pauljoegeorge <pauljoegeorge@gmail.com>
2020-04-30 20:03:59 +0900

    hiding follow for my profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
   def follow_user(me, other_user)
-    if following?(me, other_user)
-      render 'show_following_icon'
-    else
-      render 'show_follow_icon'
+    if me != other_user
+      if following?(me, other_user)
+        render 'show_following_icon'
+      else
+        render 'show_follow_icon'
+      end
     end
   end
 end
