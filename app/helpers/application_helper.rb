b7f771c5d2b71e44a0049a1f7c435a0017a298b7
pauljoegeorge <pauljoegeorge@gmail.com>
2020-05-05 13:37:46 +0900

    limiting access to other users based on follow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,26 @@
 module ApplicationHelper
-  def article_accessible?
+  def article_viewable?
     article = Article.find(params[:id])
-    if article.user.id != current_user.id
+    if !following?(current_user.id, article.user.id)
+      redirect_to(articles_path)
+    end
+  end
+
+  def article_full_access?
+    article = Article.find(params[:id])
+    if current_user.id != article.user.id
       redirect_to(articles_path)
     end
   end
 
   def vocabulary_accessible?
-    article = Vocabulary.find(params[:id])
-    if article.user.id != current_user.id
+    vocabulary = Vocabulary.find(params[:id])
+    if current_user.id != vocabulary.user.id
       redirect_to(vocabularies_path)
     end
+  end
+
+  def following?(me, other_user)
+    Follow.where(following_user_id: other_user, follower_user_id: me).first.present?
   end
 end
